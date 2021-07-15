d4939238a7746bace046f19984db6d57fc23a60d
Amin-T <a.tavakkolnia@gmail.com>
2021-07-15 12:51:28 +0200

    Codes modified

--- HTML_tagger.py
+++ HTML_tagger.py
@@ -8,6 +8,10 @@ import os
 def has_name(tag):
     return tag.has_attr('name')
 
+# Function to filter html elements having "name" and "id" attributes
+def has_name_id(tag):
+    return tag.has_attr('name') and tag.has_attr('id')
+
 # Function to detache strings
 def strDetached(string, split=True, split_by=':'):
     if split:
@@ -29,21 +33,20 @@ def strDetached(string, split=True, split_by=':'):
 
 class HTMLtagger():
 
-    def __init__(self, file, resources):
+    def __init__(self, resources):
         """
-        file: path of the HTML file to be tagged
         resources: path of the folder containing the Taxonomy (XSD) files
         """
-        self.file = file
+
         self.resources = resources
 
 
-    def LoadHTML(self, get_html=False):
+    def LoadHTML(self, file, get_html=False):
         """
         Read the HTML file.
         Returns the parsed HTML object, if "get_html" is True.
         """
-        with open(self.file, 'rb') as f:
+        with open(file, 'rb') as f:
             html_doc = f.read()
 
         parsed_html = BeautifulSoup(html_doc, 'html.parser')
@@ -74,76 +77,73 @@ class HTMLtagger():
         Tags the elements in the HTML file.
         Returns the dict of tagged id.
         """
-        divs = self.parsed_html.body.find_all('div')
-        xbrl_context = divs[0]
+        tags = self.parsed_html.body.find_all(has_name_id)
+        xbrl_context = self.parsed_html.body.find('ix:header')
         xs_element = self.xsd_dict.get('xs:element')
         Tags_dict = {}
 
-        for i in range(len(divs)):
-            tags = divs[i].find_all(has_name)
-            if tags:
-                for tag in tags:
-                    id = tag['id']
-                    name = tag['name']
-                    contextref = tag['contextref']
+        for tag in tags:
+            id = tag['id']
+            name = tag['name']
+            contextref = tag['contextref']
 
-                    Tags_dict[id] = {'Attributes': {},
-                                    'Labels': {},
-                                    'References': {},
-                                    'Calculation': {}}
+            Tags_dict[id] = {'Attributes': {},
+                            'Labels': {},
+                            'References': {},
+                            'Calculation': {}}
 
-                    xs = {}
-                    for d in xs_element:
-                        if d['@name'] == name.split(':')[-1]:
-                            xs = d
-                            break
-                            
-                    if tag.name == 'ix:nonfraction':
-                        try:
-                            fact = float(tag.string.replace(',', ''))
-                        except:
-                            fact = tag.string
-                        try:
-                            format = tag['format'].split(':')[-1]
-                        except:
-                            pass
-                        try:
-                            if tag['sign']=="-":
-                                sign = 'Negative'
-                        except:
-                            sign = 'Positive'
-                        try:
-                            measure = tag['unitref']
-                        except:
-                            pass
-                        try:
-                            scale = tag['scale']
-                        except:
-                            pass
-                        
-                        Tags_dict[id]['Attributes']['Format'] = format
-                        Tags_dict[id]['Attributes']['Measure'] = measure
-                        Tags_dict[id]['Attributes']['Scale'] = scale
-                        Tags_dict[id]['Attributes']['Sign'] = sign
+            xs = {}
+            for d in xs_element:
+                if d['@name'] == name.split(':')[-1]:
+                    xs = d
+                    break
+                    
+            if tag.name == 'ix:nonfraction':
+                try:
+                    fact = float(tag.string.replace(',', ''))
+                except:
+                    fact = tag.string
+                try:
+                    format = tag['format'].split(':')[-1]
+                except:
+                    pass
+                try:
+                    if tag['sign']=="-":
+                        sign = 'Negative'
+                except:
+                    sign = 'Positive'
+                try:
+                    measure = tag['unitref']
+                except:
+                    pass
+                try:
+                    scale = tag['scale']
+                except:
+                    pass
+                
+                Tags_dict[id]['Attributes']['Format'] = format
+                Tags_dict[id]['Attributes']['Measure'] = measure
+                Tags_dict[id]['Attributes']['Scale'] = scale
+                Tags_dict[id]['Attributes']['Sign'] = sign
 
-                    else:
-                        fact = tag.string
+            else:
+                fact = tag.string
 
-                    Tags_dict[id]['Attributes']['Tag'] = name
-                    Tags_dict[id]['Attributes']['Fact'] = fact
+            Tags_dict[id]['Attributes']['Tag'] = name
+            Tags_dict[id]['Attributes']['Fact'] = fact
 
-                    try:
-                        start = xbrl_context.find_all(id=contextref)[0].find('xbrli:startdate').text
-                        end = xbrl_context.find_all(id=contextref)[0].find('xbrli:startdate').text
-                        Tags_dict[id]['Attributes']['Period'] = start + ' to ' + end
-                    except:
-                        date = xbrl_context.find_all(id=contextref)[0].find('xbrli:instant').text
-                        Tags_dict[id]['Attributes']['Period'] = 'As of ' + end
+            try:
+                start = xbrl_context.find_all(id=contextref)[0].find('xbrli:startdate').text
+                end = xbrl_context.find_all(id=contextref)[0].find('xbrli:startdate').text
+                Tags_dict[id]['Attributes']['Period'] = start + ' to ' + end
+            except:
+                date = xbrl_context.find_all(id=contextref)[0].find('xbrli:instant').text
+                Tags_dict[id]['Attributes']['Period'] = 'As of ' + end
 
-                    if xs:
-                        Tags_dict[id]['Attributes']['Type'] = strDetached(xs.get('@type'))
+            if xs:
+                Tags_dict[id]['Attributes']['Type'] = strDetached(xs.get('@type'))
 
-                        Tags_dict[id]['Labels']['Label'] = strDetached(xs.get('@name'))
+                Tags_dict[id]['Labels']['Label'] = strDetached(xs.get('@name'))
 
         return Tags_dict
-        
+
